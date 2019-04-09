5099614fde32a91ff25cb1bf572628776e7d1fb9
quantumleeps <dan.leeper@gmail.com>
2019-04-08 22:58:28 -0400

    Add duplicate admin action for taggedequipment

--- projects/admin.py
+++ projects/admin.py
@@ -2,22 +2,28 @@ from django.contrib import admin
 from .models import Project, Category, SubCategory, Equipment, TaggedEquipment, PlantSystem, Size
 # Register your models here.
 
-class TaggedProjectAdmin(admin.ModelAdmin):
+def duplicate_query_sets(modeladmin, request, queryset):
+    for p in queryset:
+        p.pk = None
+        p.save()
+duplicate_query_sets.short_description = "Duplicate the items"
+
+class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
     list_filter = ('name', 'code')
     fields = ('name', 'code')
 
-class TaggedCategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
     list_filter = ('name', 'code')
     fields = ('name', 'code')
 
-class TaggedSubCategoryAdmin(admin.ModelAdmin):
+class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'number', 'category')
     list_filter = ('name', 'number', 'category')
     fields = ('name', 'number', 'category')
     
-class TaggedSizeAdmin(admin.ModelAdmin):
+class SizeAdmin(admin.ModelAdmin):
     list_display = ('display_string', 'code_string')
     list_filter = ('display_string', 'code_string')
     fields = ('display_string', 'code_string')
@@ -26,17 +32,19 @@ class TaggedEquipmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'project', 'equipment_category', 'equipment_subcategory')
     list_filter = ('project', 'equipment__category', 'equipment__subcategory',)
     fields = ('name', 'description', 'equipment', 'project', 'size', 'system')
+    actions = [duplicate_query_sets]
     
 class EquipmentAdmin(admin.ModelAdmin):
     list_display = ('code', 'description', 'category', 'subcategory')
     list_filter = ('category', 'subcategory')
     fields = ('description', 'category', 'subcategory')
     ordering = ['category__code', 'subcode']
+    # actions = [duplicate_query_sets]
 
-admin.site.register(Project, TaggedProjectAdmin)
-admin.site.register(Category, TaggedCategoryAdmin)
-admin.site.register(SubCategory, TaggedSubCategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(TaggedEquipment, TaggedEquipmentAdmin)
 admin.site.register(PlantSystem)
-admin.site.register(Size, TaggedSizeAdmin)
+admin.site.register(Size, SizeAdmin)
