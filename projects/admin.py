cd51774d31d16b5ceae2da41b749119e8d7b2d7b
quantumleeps <dan.leeper@gmail.com>
2019-04-08 21:49:07 -0400

    Add filter and fields to almost all admin model views

--- projects/admin.py
+++ projects/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Project, Category, SubCategory, Equipment, TaggedEquipment, PlantSystem, Size
 # Register your models here.
 
+class TaggedProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+    list_filter = ('name', 'code')
+    fields = ('name', 'code')
+
+class TaggedCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+    list_filter = ('name', 'code')
+    fields = ('name', 'code')
+
+class TaggedSubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number', 'category')
+    list_filter = ('name', 'number', 'category')
+    fields = ('name', 'number', 'category')
+    
+class TaggedSizeAdmin(admin.ModelAdmin):
+    list_display = ('display_string', 'code_string')
+    list_filter = ('display_string', 'code_string')
+    fields = ('display_string', 'code_string')
+
 class TaggedEquipmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'project', 'equipment_category', 'equipment_subcategory')
     list_filter = ('project', 'equipment__category', 'equipment__subcategory',)
@@ -13,10 +33,10 @@ class EquipmentAdmin(admin.ModelAdmin):
     fields = ('description', 'category', 'subcategory')
     ordering = ['category__code', 'subcode']
 
-admin.site.register(Project)
-admin.site.register(Category)
-admin.site.register(SubCategory)
+admin.site.register(Project, TaggedProjectAdmin)
+admin.site.register(Category, TaggedCategoryAdmin)
+admin.site.register(SubCategory, TaggedSubCategoryAdmin)
 admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(TaggedEquipment, TaggedEquipmentAdmin)
 admin.site.register(PlantSystem)
-admin.site.register(Size)
+admin.site.register(Size, TaggedSizeAdmin)
